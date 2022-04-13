6bdfb96487d172bf3df37541fc8572eb20feee55
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-04-13 19:27:38 +0300

    ft-components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { FormsModule } from '@angular/forms';
+import { HighlightDirective } from './highlight.directive';
+import { DateCountPipe } from './date-count.pipe';
+import { UpVoteDownVoteComponent } from './up-vote-down-vote/up-vote-down-vote.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    QuoteDetailsComponent,
+    NavbarComponent,
+    QuoteFormComponent,
+    HighlightDirective,
+    DateCountPipe,
+    UpVoteDownVoteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
