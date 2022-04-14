4c71e8505131f4782b7e4a735d93accb8507c8ef
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-04-14 12:04:44 +0300

    ft: quote component html template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,29 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
-import { QuoteDetailsComponent } from './quote-details/quote-details.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { QuoteFormComponent } from './quote-form/quote-form.component';
-import { FormsModule } from '@angular/forms';
-import { HighlightDirective } from './highlight.directive';
-import { DateCountPipe } from './date-count.pipe';
-import { UpVoteDownVoteComponent } from './up-vote-down-vote/up-vote-down-vote.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteComponent,
-    QuoteDetailsComponent,
-    NavbarComponent,
-    QuoteFormComponent,
-    HighlightDirective,
-    DateCountPipe,
-    UpVoteDownVoteComponent
+    QuoteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
